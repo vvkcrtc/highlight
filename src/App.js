8e3c7159d6ba4e4c43fcd547db8bbeb545d290ad
vvkcrtc <105951181+vvkcrtc@users.noreply.github.com>
2022-06-16 11:01:00 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,119 @@
+import './App.css';
+
+import React, { useState } from 'react';
+
+function New(props) {
+    return (
+        <div className="wrap-item wrap-item-new">
+            <span className="label">New!</span>
+            {props.children}
+        </div>
+    )
+};
+
+function Popular(props) {
+    return (
+        <div className="wrap-item wrap-item-popular">
+            <span className="label">Popular!</span>
+            {props.children}
+        </div>
+    )
+};
+
+function withHightLight(Component) {
+  return class extends React.Component {
+    state = {};
+    render() {
+      if (this.props.views > 1000) {
+        return (
+          <Popular><Component {...this.props} /></Popular>
+        ) 
+      } else if (this.props.views < 100) {
+        return (
+          <New><Component {...this.props} /></New>
+        ) 
+      }
+      return (
+        <Component {...this.props} />
+       )
+
+    }
+  };
+}
+    const HightLightVideo = withHightLight(Video);
+    const HightLightArticle = withHightLight(Article);
+
+function Article(props) {
+    return (
+        <div className="item item-article">
+            <h3><a href="#">{props.title}</a></h3>
+            <p className="views">Прочтений: {props.views}</p>
+        </div>
+    )
+};
+
+function Video(props) {
+    return (
+        <div className="item item-video">
+            <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <p className="views">Просмотров: {props.views}</p>
+        </div>
+    )
+};
+
+function List(props) {
+    return props.list.map(item => {
+        switch (item.type) {
+            case 'video':
+                return (
+//                    <Video {...item} />
+                      <HightLightVideo {...item} />
+                );
+
+            case 'article':
+                return (
+                    <HightLightArticle {...item} />
+//                    <Article {...item} />
+                );
+        }
+    });
+};
+
+export default function App() {
+    const [list, setList] = useState([
+        {
+            type: 'video',
+            url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+            views: 50
+        },
+        {
+            type: 'video',
+            url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+            views: 12
+        },
+        {
+            type: 'article',
+            title: 'Невероятные события в неизвестном поселке...',
+            views: 175
+        },
+        {
+            type: 'article',
+            title: 'Секретные данные были раскрыты!',
+            views: 1532
+        },
+        {
+            type: 'video',
+            url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+            views: 4253
+        },
+        {
+            type: 'article',
+            title: 'Кот Бегемот обладает невероятной...',
+            views: 12,
+        },
+    ]);
+
+    return (
+        <List list={list} />
+    );
+}
